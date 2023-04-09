f359da1c3edcef71a21123efdb5a4d44ea70b086
duan-dky <duan-dky@proton.me>
2023-04-09 19:08:08 +0800

    separate cli input from request, add openai api request and fix some prompt bugs

--- service/request.py
+++ service/request.py
@@ -6,115 +6,103 @@ from bs4 import BeautifulSoup
 import html
 import re
 import os
-import atexit
-import tty
-import termios
-from itertools import zip_longest
+from cli_input import *
 from prompt import change_style
+import traceback
 
-config_file = os.getenv('HOME')+'/.config/chatgpt-py/config.json'
-
-
-class KeyGetter:
-    def arm(self):
-        self.old_term = termios.tcgetattr(sys.stdin)
-        tty.setcbreak(sys.stdin)
-
-        atexit.register(self.disarm)
-
-    def disarm(self):
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.old_term)
-
-    def getch(self):
-        self.arm()
-        ch = sys.stdin.read(1)[0]
-        self.disarm()
-        return ch
+config_azure_file = os.getenv('HOME')+'/.config/chatgpt-py/config_azure.json'
+config_openai_file = os.getenv('HOME')+'/.config/chatgpt-py/config_openai.json'
 
 
-def print_question(message: str) -> None:
-    print("\033[92m" + message + "\033[0m", flush=True)
-
-
-def ia_selection(question: str, options: list = None, flags: list = None) -> str:
-    print_question(question)
-    return _draw_ia_selection(options, flags)
-
-
-def _draw_ia_selection(options: list, flags: list = None):
-    __UNPOINTED = " "
-    __POINTED = ">"
-    __INDEX = 0
-    __LENGTH = len(options)
-    __ARROWS = __UP, _ = 65, 66
-    __ENTER = 10
-
-    if flags is None:
-        flags = []
-
-    def _choices_print():
-        for i, (option, flag) in enumerate(zip_longest(options, flags, fillvalue='')):
-            if i == __INDEX:
-                print(f" {__POINTED} {{0}}{option} {flag}{{1}}".format(
-                    '\033[94m', '\033[0m'))
-            else:
-                print(f" {__UNPOINTED} {option} {flag}")
-
-    def _choices_clear():
-        print(f"\033[{__LENGTH}A\033[J", end='')
-
-    def _move_pointer(ch_ord: int):
-        nonlocal __INDEX
-        __INDEX = max(
-            0, __INDEX - 1) if ch_ord == __UP else min(__INDEX + 1, __LENGTH - 1)
-
-    def _main_loop():
-        kg = KeyGetter()
-        _choices_print()
-        while True:
-            key = ord(kg.getch())
-            if key in __ARROWS:
-                _move_pointer(key)
-            _choices_clear()
-            _choices_print()
-            if key == __ENTER:
-                _choices_clear()
-                _choices_print()
-                break
-
-    _main_loop()
-    return options[__INDEX]
-
-
-def init(config_file):
-    if not os.path.exists(config_file):
+def init_azure(config_azure_file):
+    if not os.path.exists(config_azure_file):
         api_type = input("api_type: ")
         api_base = input("api_base: ")
         api_version = input("api_version: ")
         api_key = input("api_key: ")
+        engine = input("engine: ")
+        max_tokens = input("max_tokens: ")
+        temperature = input("temperature: ")
+        timeout = input("timeout: ")
         config = {
             "api_type": api_type,
             "api_base": api_base,
             "api_version": api_version,
-            "api_key": api_key
+            "api_key": api_key,
+            "engine": engine,
+            "max_tokens": int(float(max_tokens)),
+            "temperature": float(temperature),
+            "timeout": int(timeout)
         }
         os.system("mkdir -p "+os.getenv('HOME')+'/.config/chatgpt-py')
-        with open(config_file, 'w') as f:
+        with open(config_azure_file, 'w') as f:
             f.write(json.dumps(config))
 
 
-init(config_file)
-with open(config_file, 'r') as f:
-    try:
-        config = json.load(f)
-    except ValueError or TypeError:
-        print("Invalid json, please input your azure openai service config and try again.")
-        init(config_file)
+def init_openai(config_openai_file):
+    if not os.path.exists(config_openai_file):
+        api_key = input("api_key: ")
+        openai.api_key = api_key
+        i = 0
+        available_engine = []
+        available_list = openai.Engine.list()['data']
+        while (i < len(available_list)):
+            available_engine.append(available_list[i]['id'])
+            i += 1
+        print("Available engines:")
+        i = 0
+        while i < len(available_engine):
+            print("\033[92m" + str(i+1) + "\033[0m", end=": ")
+            print(available_engine[i])
+            i += 1
+        engine = input("\033[92m" +
+                       "Which engine would you like to use? \n" + "\033[0m")
+        engine = available_engine[int(engine)-1]
+        max_tokens = input("max_tokens: ")
+        temperature = input("temperature: ")
+        timeout = input("timeout: ")
+        config = {
+            "api_type": "openai",
+            "api_key": api_key,
+            "engine": engine,
+            "max_tokens": int(float(max_tokens)),
+            "temperature": float(temperature),
+            "timeout": int(timeout)
+        }
+        os.system("mkdir -p "+os.getenv('HOME')+'/.config/chatgpt-py')
+        with open(config_openai_file, 'w') as f:
+            f.write(json.dumps(config))
 
-openai.api_type = config['api_type']
-openai.api_base = config['api_base']
-openai.api_version = config['api_version']
-openai.api_key = config['api_key']
+
+api_type = ia_selection("What's your OpenAI API type?",
+                        options=["Azure", "OpenAI"],
+                        flags=[f"~",
+                               f"~"])
+match(api_type):
+    case "Azure":
+        init_azure(config_azure_file)
+        with open(config_azure_file, 'r') as f:
+            try:
+                config = json.load(f)
+            except ValueError or TypeError:
+                print(
+                    "Invalid json, please input your azure openai service config and try again.")
+                init_azure(config_azure_file)
+
+        openai.api_type = config['api_type']
+        openai.api_base = config['api_base']
+        openai.api_version = config['api_version']
+        openai.api_key = config['api_key']
+    case "OpenAI":
+        init_openai(config_openai_file)
+        with open(config_openai_file, 'r') as f:
+            try:
+                config = json.load(f)
+            except ValueError or TypeError:
+                print(
+                    "Invalid json, please input your openai service config and try again.")
+                init_openai(config_openai_file)
+        openai.api_key = config['api_key']
 
 try:
     messages = []
@@ -128,16 +116,16 @@ try:
         messages.append(
             {"role": "user", "content": prompt[1]+"user: "+user_input+" "+prompt[0]+": "})
         response = openai.ChatCompletion.create(
-            engine="my-gpt3-model",
+            engine=config['engine'] if config['api_type'] == "azure" else None,
+            model=config['engine'] if config['api_type'] == "openai" else None,
             messages=messages,
-            temperature=0.7,
-            max_tokens=int(4e3),
+            temperature=config['temperature'],
+            max_tokens=int(config['max_tokens']),
             # top_p=1,
             # frequency_penalty=0,
             # presence_penalty=0,
-            stop=["user: ", prompt[0]+": ", "Task: ", "<|im_end|>"],
-            timeout=120,
-            n=1
+            stop=["user: ", prompt[0]+": ", "<|im_end|>"],
+            timeout=int(config['timeout'])
         )
         chat_response = response
         answer = chat_response['choices'][0]['message']['content']
@@ -153,5 +141,6 @@ try:
         print(prompt[0]+f': \n{html.unescape(soup.get_text())}')
         messages.append(
             {"role": prompt[0], "content": html.unescape(soup.get_text())})
-except KeyboardInterrupt:
+except Exception or KeyboardInterrupt:
+    traceback.print_exc()
     sys.exit(0)
