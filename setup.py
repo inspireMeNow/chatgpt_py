db02c2e20e789ed9bbab401170b1819632017900
duan <74866582+duan-dky@users.noreply.github.com>
2023-04-10 01:06:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='chatgpt_py',
-    version='0.4',
+    version='0.1',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
